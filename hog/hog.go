d2928ece829b78c423ba8331fee7ccbdb2f33db2
yuqing.ji <yuqing.ji@outlook.com>
2020-05-12 09:39:49 +0800

    update

--- hog/hog.go
+++ hog/hog.go
@@ -1,7 +1,18 @@
-package hog
+package main
 
 import "os/exec"
 
+func hog(url string) {
+    exec.Command("open", url).Run() // mac
+}
+
 func main() {
-    exec.Command("open", "https://www.bing.com/search?q=a").Run() // mac
+    tunnels := []string{
+        "https://www.bing.com/search?q=a",
+        "https://www.bing.com/search?q=ab",
+        "https://www.bing.com/search?q=ac",
+    }
+    for _, tunnel := range tunnels {
+        hog(tunnel)
+    }
 }
